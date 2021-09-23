39e7951a74456306dc790de84b436ad87e01e7fb
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2021-09-23 15:36:58 +0900

    4주차 실습완료

--- src/com/todo/service/TodoSortByDate.java
+++ src/com/todo/service/TodoSortByDate.java
@@ -2,11 +2,10 @@ package com.todo.service;
 import java.util.Comparator;
 
 import com.todo.dao.TodoItem;
-
 public class TodoSortByDate implements Comparator<TodoItem> {
-    @Override
+    @Override 
     public int compare(TodoItem o1, TodoItem o2) {
-        return o1.getCurrent_date().compareTo(o2.getCurrent_date());
-
+        return o2.getCurrent_date().compareTo(o1.getCurrent_date());
+ 
     }
 }
