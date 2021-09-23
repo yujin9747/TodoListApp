005beed61080ed8584411e6f0fac7c16cd4ef36e
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2021-09-23 15:34:32 +0900

    4주차 실습완료

--- src/com/todo/dao/TodoItem.java
+++ src/com/todo/dao/TodoItem.java
@@ -1,20 +1,26 @@
-package com.todo.dao;
-
+package com.todo.dao; 
+ 
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
 public class TodoItem {
     private String title;
     private String desc;
-    private Date current_date;
+    private String current_date;
 
-
+ 
     public TodoItem(String title, String desc){
         this.title=title;
         this.desc=desc;
-        this.current_date=new Date();
+        SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd kk:mm:ss") ;
+        this.current_date=f.format(new Date()) ;
     }
     
-    public String getTitle() {
+    public String toSaveString() {
+    	return title + "##" + desc + "##" + current_date + "\n" ;
+    }
+
+
+	public String getTitle() {
         return title;
     }
 
@@ -30,11 +36,12 @@ public class TodoItem {
         this.desc = desc;
     }
 
-    public Date getCurrent_date() {
+    //current_date를 string으로 다룰 수 있도록 아래의 두 함수의 리턴타입과 메소드 타입을 고쳐주었다
+    public String getCurrent_date() {
         return current_date;
     }
 
-    public void setCurrent_date(Date current_date) {
+    public void setCurrent_date(String current_date) {
         this.current_date = current_date;
     }
 }
