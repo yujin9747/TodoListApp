8e25502bf8e16cc64b82ea5f7a798891d73ca592
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2021-09-23 15:37:24 +0900

    4주차 실습완료

--- src/com/todo/service/TodoSortByName.java
+++ src/com/todo/service/TodoSortByName.java
@@ -8,6 +8,6 @@ public class TodoSortByName implements Comparator<TodoItem> {
     @Override
     public int compare(TodoItem o1, TodoItem o2) {
         return o1.getTitle().compareTo(o2.getTitle());
-
+  
     }
-}
+} 
