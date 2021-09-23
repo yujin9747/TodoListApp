4cb245b24ae941650a2d18ab7b64c8cdc124ac08
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2021-09-23 15:33:38 +0900

    4주차 실습완료

--- src/com/todo/TodoMain.java
+++ src/com/todo/TodoMain.java
@@ -1,22 +1,22 @@
 package com.todo;
-
+  
+import java.io.IOException;
 import java.util.Scanner;
-
 import com.todo.dao.TodoList;
 import com.todo.menu.Menu;
-import com.todo.service.TodoUtil;
-
+import com.todo.service.TodoUtil; 
 public class TodoMain {
-	
-	public static void start() {
-	
-		Scanner sc = new Scanner(System.in);
+	public static void start() throws IOException {
+		Scanner sc = new Scanner(System.in); 
 		TodoList l = new TodoList();
 		boolean isList = false;
 		boolean quit = false;
+		TodoUtil.loadList(l, "todolist.txt") ;
+		Menu.displaymenu();
 		do {
-			Menu.displaymenu();
+	
 			isList = false;
+			Menu.prompt() ;
 			String choice = sc.next();
 			switch (choice) {
 
@@ -37,17 +37,20 @@ public class TodoMain {
 				break;
 
 			case "ls_name_asc":
+				System.out.println("제목을 오름차순으로 정렬합니다.") ;
 				l.sortByName();
 				isList = true;
 				break;
 
 			case "ls_name_desc":
+				System.out.println("제목을 역순으로 정렬합니다.") ;
 				l.sortByName();
 				l.reverseList();
 				isList = true;
 				break;
 				
 			case "ls_date":
+				System.out.println("항목이 입력된 순서대로 정렬합니다.") ;
 				l.sortByDate();
 				isList = true;
 				break;
@@ -55,13 +58,19 @@ public class TodoMain {
 			case "exit":
 				quit = true;
 				break;
-
+				
+			case "help" :
+				Menu.displaymenu();
+				break ;
 			default:
-				System.out.println("please enter one of the above mentioned command");
+				System.out.println("잘못 입력하셨습니다! 관리 명령어 사용법이 필요하시면 help를 입력해주세요");
+				choice = sc.next();
 				break;
 			}
 			
 			if(isList) l.listAll();
 		} while (!quit);
+		TodoUtil.saveList(l, "todolist.txt") ;
+		sc.close() ;
 	}
 }
