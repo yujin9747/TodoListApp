f52cb9f096af51b6d3d13c989450bfe872f0dcbd
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2021-09-26 21:13:29 +0900

    4주차실습

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -5,12 +5,16 @@ public class Menu {
         System.out.println();
         System.out.println("TodoList 관리 명령어 사용법");
         System.out.println("add - 항목 추가하기");
-        System.out.println("del - 항목 삭제하기 )");
+        System.out.println("del - 항목 삭제하기");
         System.out.println("edit - 항목 수정하기");
         System.out.println("ls - 전체 목록 보기");
         System.out.println("ls_name_asc - 목록을 이름순으로 정렬하기(오름차순)");
         System.out.println("ls_name_desc - 목록을 이름순으로 정렬하기(내림차순)");
         System.out.println("ls_date - 목록을 항목이 추가된 순서대로 정렬하기");
+        System.out.println("ls_date_desc - 목룍을 항목이 추가된 역순으로 정렬하기");
+        System.out.println("ls_cat - 카테고리 정렬하기");
+        System.out.println("find (키워드 입력) - 키워드로 검색하기");
+        System.out.println("find_cat (키워드 입력) - 키워드로 카테고리 검색하기");
         System.out.println("exit - 종료");
     }
     public static void prompt() {
